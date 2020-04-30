589e51b5ca8e74fa01c8bfe99146ba5ba2bf4913
taiki <52108527+taiki1280@users.noreply.github.com>
2020-04-30 14:33:59 +0900

    [変更]javascriptをjQueryに

--- js/tab.js
+++ js/tab.js
@@ -1,19 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // タブに対してクリックイベントを適用
-  const tabs = document.getElementsByClassName("タブ");
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", tabSwitch);
+// jQueryメソッド
+jQuery(function () {
+  // ロード時
+  var index = $.cookie("選択されたタブ")
+  if (index == undefined) {
+    index = 0;
   }
-  // タブをクリックすると実行する関数
-  function tabSwitch() {
-    // タブのclassの値を変更
-    document.getElementsByClassName("選択中")[0].classList.remove("選択中");
-    this.classList.add("選択中");
-    // コンテンツのclassの値を変更
-    document.getElementsByClassName("現在表示中")[0].classList.remove("現在表示中");
-    const arrayTabs = Array.prototype.slice.call(tabs);
-    const index = arrayTabs.indexOf(this);
-    document.getElementsByClassName("ページ")[index].classList.add("現在表示中");
-  };
+  $(".タブ").eq(index).addClass('選択中')
+  $(".ページ").eq(index).addClass('現在表示中')
+
+  //訪問回数の処理
+  var visit_count = $.cookie("visit_count");
+  if (visit_count == undefined) {
+    visit_count = 0;
+  }
+  visit_count++;
+  $.cookie("visit_count", visit_count, { expires: 1 });
+  $("#welcome").text(visit_count + "回目の訪問ありがとう☆");
+
 });
 
+// タブがクリックされた時
+$('.タブ').click(function () {
+  // クリックされたタブの順番をindex変数へ
+  let index = $('.タブ').index(this);
+  $.cookie("選択されたタブ", index)
+  switch_tab(index)
+});
+
+// タブの入れ替えメソッド
+function switch_tab(index) {
+  // 選択中のタブを非選択にし、内容を非表示
+  $('.タブ').removeClass('選択中');
+  $('.ページ').removeClass('現在表示中')
+  // 選択されたタブを選択中にし、内容を表示
+  $('.タブ').eq(index).addClass('選択中');
+  $('.ページ').eq(index).addClass('現在表示中');
+};
---
+++ js/消滅都市.js
@@ -0,0 +1,11 @@
+
+// var array = [1, 2, 3]
+
+for (let i = 1; i < 90; i++) {
+  $("#消滅都市").append("<div class=あ>aai</div>")
+}
+
+$(".あ:nth-child(1)").css({
+  "color": "#fff",
+  "background": "black"
+})
