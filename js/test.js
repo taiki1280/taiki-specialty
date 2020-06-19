c03510d93a06783cb30a19ea248c9a9fe7952cb7
taiki <52108527+taiki1280@users.noreply.github.com>
2020-06-19 19:53:22 +0900

    [変更]vue.jsタブ切り替え半分完成

--- js/test.js
+++ js/test.js
@@ -16,17 +16,38 @@ new Vue({
 
     // Cookieに保存する
     this.visited_cnt = this.$cookies.get("visited_cnt")
-    if (this.visited_cnt == undefined)
+    if (this.visited_cnt === undefined)
       this.$cookies.set("visited_cnt", 0)
     this.visited_cnt++;
     this.$cookies.set("visited_cnt", this.visited_cnt)
   }
 })
 
-  // タブの切り替え
-// let nav = new Vue({
-//   el: "#nav",
-//   data: {
-//     test: "aasdffw"
-//   }
-// })
+// タブの切り替え
+let nav = new Vue({
+  el: "#nav",
+  data: {
+    now_tab: "ホーム",
+    // menus: ["ホーム", "学校", "趣味", "意見", "Twitter",]
+    menus: ["ホーム", "Twitter"]
+  },
+  mounted: function () {
+    this.now_tab = this.$cookies.get("now_tab")
+    if (this.now_tab === undefined)
+      this.$cookies.set("now_tab", "ホーム")
+  },
+  methods: {
+    switch_tab: function (menu) {
+      this.$cookies.set("now_tab", menu)
+      this.now_tab = menu
+      main.now_tab = menu
+    }
+  }
+})
+
+let main = new Vue({
+  el: "#main",
+  data: {
+    now_tab: "ホーム"
+  },
+})
