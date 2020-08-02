b35c26f0020755451671778ce8dcd38cb4fc177e
taiki <52108527+taiki1280@users.noreply.github.com>
2020-08-02 20:34:31 +0900

    [test]お試し

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,73 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        taiki1280.github.io
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <h1 class="title">たいきのほ～むぺ～じ</h1>
+    <Logo />
+    <div class="links">
+      <a
+        href="https://github.com/taiki1280"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="button grey"
+      >GitHub</a>
+      <a
+        href="https://twitter.com/taiki_global"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="button cyan"
+      >Twitter</a>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass">
+.container
+  min-height: 100vh
+  display: flex
+  flex-direction: column
+  justify-content: space-evenly
+  align-items: center
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.title
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
+  display: block
+  font-weight: 300
+  font-size: (100vmin / 10) * 0.9
+  color: #35495e
+  letter-spacing: 1px
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+.links
+  width: 90%
+  height: (100vmax / 10)
+  display: flex
+  justify-content: space-around
+  align-items: center
+  background: rgba(#fff ,.8)
+  border-radius: 5vmin
+  .button
+    text-decoration: none
+    display: flex
+    justify-content: center
+    align-items: center
+    height: (100vmax / 10) * 0.8
+    width: (100vmax / 10) * 0.8
+    border-radius: 50%
+    &:hover
+      color: #fff
+  @mixin button($color)
+    border: 1px solid $color
+    color: $color
+    &:hover
+      background-color: $color
+  .green
+    +button(green)
+  .grey
+    +button(grey)
+  .cyan
+    +button(cyan)
 </style>
