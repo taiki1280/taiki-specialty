b4a31692f59b42de96db1d8448b5b5e245a72ca4
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-02 10:01:15 +0900

    Update Numer0n.vue

--- pages/Numer0n.vue
+++ pages/Numer0n.vue
@@ -38,7 +38,7 @@ div
     // default(0.6, #fff)
     display flex
     flex-direction column
-    justify-content start
+    justify-content flex-start
     align-items flex-start
     .result
       width 100%
